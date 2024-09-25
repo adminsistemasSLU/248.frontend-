cacff977088b2afc31edd9de550d9caa7d784bd2
Israel0305 <jrodrigueza_0503@outlook.com>
2024-09-25 00:57:06 -0500

    correccion en proteccion de rutas

--- src/services/RoutesService/RequireAuth.jsx
+++ src/services/RoutesService/RequireAuth.jsx
@@ -2,11 +2,23 @@
 import React from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import { useAuth } from '../AuthProvider'; // Asegúrate de importar useAuth correctamente
+import { PERMISSIONS_STORAGE_KEY } from '../../utils/constantes';
 
-const RequireAuth = ({ children }) => {
+const RequireAuth = ({ children, requiredBaldosaId  }) => {
   const { user } = useAuth(); // Accede al estado de usuario desde el contexto
   const location = useLocation(); // Hook para obtener la ubicación actual
   const { isLoading } = useAuth();
+  
+// Obtener permisos del localStorage
+const storedBaldosasPermiso = localStorage.getItem(PERMISSIONS_STORAGE_KEY);
+const baldosasPermiso = storedBaldosasPermiso ? JSON.parse(storedBaldosasPermiso) : { Baldosas: [] };
+
+  // Verificar permisos
+  const hasPermission = requiredBaldosaId ? baldosasPermiso.Baldosas.includes(requiredBaldosaId.toString()) : true;
+
+  if (requiredBaldosaId && !hasPermission) {
+    return <Navigate to="/not-authorized" replace />; // Redirigir si no tiene permiso
+  }
   
   if (isLoading) {
     return <div>Loading...</div>;
