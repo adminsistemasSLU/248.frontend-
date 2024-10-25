5f15583a7579c50cfd92240dfc3a1c716db58f8b
Israel0305 <jrodrigueza_0503@outlook.com>
2024-10-25 02:40:10 -0500

    depuracion

--- src/components/QuoterLife/personalFormLife.jsx
+++ src/components/QuoterLife/personalFormLife.jsx
@@ -993,6 +993,7 @@ const PersonalFormLife = forwardRef((props, ref) => {
   };
 
   function verificarLavadoActivo(cedulaData) {
+    debugger;
     setErrorCedula(true);
     setOpen(true);
     setmessageError(cedulaData.message);
