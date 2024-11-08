58b73565a13011232b257ccf54cbb05148406105
Israel0305 <jrodrigueza_0503@outlook.com>
2024-11-07 23:47:23 -0500

    correccion en validacion

--- src/components/QuoterLife/steppersLife.jsx
+++ src/components/QuoterLife/steppersLife.jsx
@@ -310,8 +310,12 @@ export default function SteppersLife() {
     setIsVisibleCertificado(true);
     setIsVisibleFormulario(true);
     debugger;
-    let dato =  Object.values(JSON.parse(DocumentosVida)).some(doc => doc.nombre === "Ninguno");
+   
     let DocumentosVida = sessionStorage.getItem(LS_DOCUMENTOSVIDA);
+
+
+    let dato =  Object.values(JSON.parse(DocumentosVida)).some(doc => doc.nombre === "Ninguno");
+    
     if (
       !DocumentosVida ||
       (Array.isArray(JSON.parse(DocumentosVida)) && 
