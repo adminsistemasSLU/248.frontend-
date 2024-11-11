3a947096326d78d198a612bb416b18501edd22d1
Israel0305 <jrodrigueza_0503@outlook.com>
2024-11-11 11:11:39 -0500

    quitar icono de orndenamiento

--- src/modules/Quoter/myQuoters.jsx
+++ src/modules/Quoter/myQuoters.jsx
@@ -232,6 +232,7 @@ function EnhancedTableHead(props) {
               active={orderBy === headCell.id}
               direction={orderBy === headCell.id ? order : "asc"}
               onClick={createSortHandler(headCell.id)}
+              hideSortIcon={true}
             >
               {headCell.label}
               {orderBy === headCell.id ? (
